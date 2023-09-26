d7577f61ad77aa4bf188543f2edc0a326e6b2913
Ab2nour <61651582+Ab2nour@users.noreply.github.com>
2023-09-26 19:41:29 +0200

    refactor: add constants.py file with column names
    And replace column names with imported variable

---
+++ src/constants.py
@@ -0,0 +1,43 @@
+var_numeriques = [
+    "Year_Birth",
+    "Income",
+    "Recency",
+    "MntWines",
+    "MntFruits",
+    "MntMeatProducts",
+    "MntFishProducts",
+    "MntSweetProducts",
+    "MntGoldProds",
+    "NumDealsPurchases",
+    "NumWebPurchases",
+    "NumCatalogPurchases",
+    "NumStorePurchases",
+    "NumWebVisitsMonth",
+]
+
+var_categoriques_original = [
+    "Education",
+    "Marital_Status",
+    "Kidhome",
+    "Teenhome",
+    "Complain",
+    "AcceptedCmp1",
+    "AcceptedCmp2",
+    "AcceptedCmp3",
+    "AcceptedCmp4",
+    "AcceptedCmp5",
+    "Response",
+]
+
+var_categoriques = [  # Complain was removed
+    "Education",
+    "Marital_Status",
+    "Kidhome",
+    "Teenhome",
+    "AcceptedCmp1",
+    "AcceptedCmp2",
+    "AcceptedCmp3",
+    "AcceptedCmp4",
+    "AcceptedCmp5",
+    "Response",
+]
